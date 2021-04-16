a10392c700df2a12db0ab6aa8e0fd6165bc23dbd
Tawanda Msengezi <xrthemc@gmail.com>
2021-04-16 10:33:54 +0300

    Created Post request with new User - Me

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
